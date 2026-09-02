26dd9d18d6369e0783415b90579aec7a33ef50bb
savitapandey1855-ship-it <savitapandey1855@gmail.com>
2026-09-02 16:41:46 +0530

    Add app build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.domino.social"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.domino.social"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.browser:browser:1.8.0")
+    implementation("com.google.firebase:firebase-messaging:24.0.0")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+}
